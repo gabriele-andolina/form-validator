d92fb3ea4d06595f4aa6a34ae3d122c9858bbf63
gabriele-andolina <gabe.andolina@gmail.com>
2023-06-18 22:36:19 +0200

    Added username validation

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
 
+// Event Listeners
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log("submit");
+
+    if (username.value === "") {
+        showError(username, "Username is required");
+    } else {
+        showSuccess(username);
+    }
 });
