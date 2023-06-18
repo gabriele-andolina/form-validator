c8708aee14815ed54b5ca983a2349feb8919cd1b
gabriele-andolina <gabe.andolina@gmail.com>
2023-06-18 22:24:38 +0200

    Created form DOM variables and submit eventListener

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hey there!");
+const form = document.querySelector("#form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("submit");
+});
