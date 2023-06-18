c0f1c782894502ffa3ac862c3b3d39ddcaef6363
gabriele-andolina <gabe.andolina@gmail.com>
2023-06-18 22:37:12 +0200

    Created showError() to notify user of input-specific error

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirm-password");
 
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = "form-control error";
+    const small = formControl.querySelector("small");
+    small.innerText = message;
+}
+
 // Event Listeners
 form.addEventListener("submit", function (e) {
     e.preventDefault();
